7997bc829d60a2e331e658c829ea0b4d0235edb7
HyperDunk <aakarsh.mhm@gmail.com>
2013-10-24 03:47:32 -0700

    same as previous with globals.h and config

--- kernel/include/globals.h
+++ kernel/include/globals.h
@@ -5,3 +5,7 @@
 
 extern kthread_t *curthr;
 extern proc_t *curproc;
+
+extern void *testproc(int arg1, void *arg2);
+extern void *sunghan_deadlock_test(int arg1, void *arg2);
+extern void *sunghan_test(int arg1, void *arg2);
